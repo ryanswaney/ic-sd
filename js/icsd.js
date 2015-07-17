34ec66b770c6681ef06fd127e4c96631e6820559
Ryan Swaney <ryan.swaney@gmail.com>
2015-07-17 16:32:35 -0400

    Updated speaker bio js/css.

--- js/icsd.js
+++ js/icsd.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
   // Expand/Collapse for Speaker bios
   $('div.speakers h2 span.more').on('click', function() {
 
-    $(this).next('ul').toggleClass('toggled');
+    $(this).parent().next('.speaker-list').toggleClass('toggled');
 
   });
 
